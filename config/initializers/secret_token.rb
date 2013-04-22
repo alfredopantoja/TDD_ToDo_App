d656257a1b397bf780df7f2a50f9a0b45485c07a
Alfredo Pantoja <chololoy@gmail.com>
2013-04-22 15:45:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddTodo::Application.config.secret_key_base = '9699fbd2f633d50fbc16b6ac20fd1bbf3e47d6625fe7c50252230e54224997d567a8be89da213f7dedf5163eba4914e00daea5d6e6cd2b90ebc9bf5b3eeec429'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TddTodo::Application.config.session_store :cookie_store, key: '_tdd_todo_session'
